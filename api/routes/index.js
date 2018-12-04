3a9ccce591330f017c1e537625e1d626fe79d909
prashant.gangwar <prashant.gangwar@sofocle.com>
2018-12-04 19:58:07 +0530

    routes added

--- api/routes/index.js
+++ api/routes/index.js
@@ -10,13 +10,11 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static('./views'));
 app.use(bodyParser());
 
-
 var session = require('express-session');
 var cookieParser = require('cookie-parser')
 
 app.use(cookieParser('secret'))
 app.use(session({ secret: 'prashant', saveUninitialized: true, resave: true, cookie: { maxAge: 30000 } }));
-
 
 var urlString = 'http://crypto-testnets.sofodev.co:8000';
 var server = new StellarSdk.Server(urlString, { allowHttp: true });
@@ -52,21 +50,17 @@ var freezedAccount = {
 }
 var keypairFreezed = StellarSdk.Keypair.fromSecret(freezedAccount.secret);
 
-
 var assetName = 'okToken';
 var okToken = new StellarSdk.Asset(
     assetName, issuer.publicKey);
-
 
 router.get('/login', function (req, res) {
     res.render('login.ejs');
 });
 
-
 router.post('/login', function (req, res) {
     var pub = req.body.publickey;
     console.log(req.body.publickey);
-
     //session
     req.session.publickey = req.body.publickey;
     //session
@@ -82,8 +76,6 @@ router.post('/login', function (req, res) {
             console.log(error);
         });
 });
-
-
 
 router.post('/logout', urlencodedParser, function (req, res) {
     req.session.destroy();
@@ -114,13 +106,17 @@ router.get('/balanceOf', function (req, res) {
 
 
 router.get('/loadAccount', async function (req, res) {
-    keypair = StellarSdk.Keypair.fromSecret('SDHOAMBNLGCE2MV5ZKIVZAQD3VCLGP53P3OBSBI6UN5L5XZI5TKHFQL4')
-    var pubkey = keypair.publicKey();
-    var loadAccount = await server.loadAccount(pubkey);
-    console.log("load account", loadAccount);
-    console.log('###############################');
-
-    res.send(loadAccount);
+    try {
+        keypair = StellarSdk.Keypair.fromSecret('SDHOAMBNLGCE2MV5ZKIVZAQD3VCLGP53P3OBSBI6UN5L5XZI5TKHFQL4')
+        var pubkey = keypair.publicKey();
+        var loadAccount = await server.loadAccount(pubkey);
+        console.log("load account", loadAccount);
+        console.log('###############################');
+        res.send(loadAccount);
+    }
+    catch (e) {
+        console.log("error occured")
+    }
 });
 
 
@@ -185,7 +181,6 @@ router.get('/transferAsset', async function (req, res) {
 
 
 app.get('/authorizationFlag', function (req, res) {
-
     server.loadAccount(issuerPublicKey)
         .then(function (account) {
             StellarSdk.Network.useTestNetwork();
@@ -197,15 +192,12 @@ app.get('/authorizationFlag', function (req, res) {
             console.log(account);
             transaction.sign(issuingKeys);
             server.submitTransaction(transaction);
-
             res.send('flag set');
-
         })
 });
 
 
-router.get('/changeTrust', function (req, res) {// change trustline and make tx
-
+router.get('/changeTrust', function (req, res) {
     var issuingKeys = StellarSdk.Keypair
         .fromSecret(issuerSecretKey);
     var receivingKeys = StellarSdk.Keypair
@@ -214,8 +206,6 @@ router.get('/changeTrust', function (req, res) {// change trustline and make tx
     server.loadAccount(receivingKeys.publicKey())
         .then(function (receiver) {
             var transaction = new StellarSdk.TransactionBuilder(receiver)
-                // The `changeTrust` operation creates (or alters) a trustline
-                // The `limit` parameter below is optional
                 .addOperation(StellarSdk.Operation.changeTrust({
                     asset: okToken,
                     limit: '1000'// optional if not set thn unlimited 
@@ -300,22 +290,27 @@ router.get('/burn', async function (req, res) {
 
 
 router.get('/freezeBurnAccount', async function (req, res) {
-    var keypairFreezed = StellarSdk.Keypair.fromSecret(freezedAccount.secret);
-    const thresholds = {
-        masterWeight: 0, // no rights
-        lowThreshold: 1,
-        medThreshold: 2, // payment is medium threshold
-        highThreshold: 2
+    try {
+        var keypairFreezed = StellarSdk.Keypair.fromSecret(freezedAccount.secret);
+        const thresholds = {
+            masterWeight: 0, // no rights
+            lowThreshold: 1,
+            medThreshold: 2, // payment is medium threshold
+            highThreshold: 2
+        }
+        console.log(escrowKeyPair.publicKey());
+        var freezedAccount = await server.loadAccount(freezedAccount.publicKey);
+        console.log('account info', freezedAccount);
+        let transaction = new StellarSdk.TransactionBuilder(freezedAccount)
+            .addOperation(StellarSdk.Operation.setOptions(thresholds))
+            .build()
+        transaction.sign(keypairFreezed);
+        await server.submitTransaction(transaction);
+        console.log('done');
     }
-    console.log(escrowKeyPair.publicKey());
-    var freezedAccount = await server.loadAccount(freezedAccount.publicKey);
-    console.log('account info', freezedAccount);
-    let transaction = new StellarSdk.TransactionBuilder(freezedAccount)
-        .addOperation(StellarSdk.Operation.setOptions(thresholds))
-        .build()
-    transaction.sign(keypairFreezed);
-    await server.submitTransaction(transaction);
-    console.log('done');
+    catch (e) {
+        console.log("error occured")
+    }
 });
 
 
@@ -323,49 +318,59 @@ router.get('/freezeBurnAccount', async function (req, res) {
 //check before sending payments reciever is aut to recv assets
 
 router.get('/checkIsAuthorized', function (req, res) {
-    var okTokenCode = 'okToken';
-    var accountId = 'GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5';
-    //recievers public key
-    server.loadAccount(accountId).then(function (account) {
-        var trusted = account.balances.some(function (balance) {
-            return balance.asset_code === okTokenCode &&
-                balance.asset_issuer === issuerPublicKey;
+    try {
+        var okTokenCode = 'okToken';
+        var accountId = 'GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5';
+        //recievers public key
+        server.loadAccount(accountId).then(function (account) {
+            var trusted = account.balances.some(function (balance) {
+                return balance.asset_code === okTokenCode &&
+                    balance.asset_issuer === issuerPublicKey;
+            });
+            console.log(trusted ? 'Trusted user' : 'Not trusted ');
+            res.send(trusted ? 'Trusted user' : 'Not trusted ')
         });
-        console.log(trusted ? 'Trusted user' : 'Not trusted ');
-        res.send(trusted ? 'Trusted user' : 'Not trusted ')
-    });
+    }
+    catch (e) {
+        console.log("error occured")
+    }
 });
 
 
 router.get('/streampayments', function (req, res) {
-    let funding_account = issuer.publicKey // This needs to be a valid account on the network and should return an Account object
-    let stream = server.payments()
-        .forAccount(funding_account)
-        .cursor('now')
-        .stream({
-            onmessage: function (message) {
-                if ((message.type == "create_account") || (message.type == "payment")) {
-                    var details = "from:" + message.from + ",to:" + message.to + ",amount:" + message.amount;
-                    console.log(details)
-                    // Set your own code after we find our create account event
-                    stream()
+    try {
+        let funding_account = issuer.publicKey // This needs to be a valid account on the network and should return an Account object
+        let stream = server.payments()
+            .forAccount(funding_account)
+            .cursor('now')
+            .stream({
+                onmessage: function (message) {
+                    if ((message.type == "create_account") || (message.type == "payment")) {
+                        var details = "from:" + message.from + ",to:" + message.to + ",amount:" + message.amount;
+                        console.log(details)
+                        // Set your own code after we find our create account event
+                        stream()
+                    }
                 }
-            }
-        })
+            })
 
         let stream2 = server.payments()
-        .forAccount(account2.publicKey)
-        .cursor('now')
-        .stream({
-            onmessage: function (message) {
-                if ((message.type == "create_account") || (message.type == "payment")) {
-                    var details = "from:" + message.from + ",to:" + message.to + ",amount:" + message.amount;
-                    console.log(details)
-                    // Set your own code after we find our create account event
-                    stream()
+            .forAccount(account2.publicKey)
+            .cursor('now')
+            .stream({
+                onmessage: function (message) {
+                    if ((message.type == "create_account") || (message.type == "payment")) {
+                        var details = "from:" + message.from + ",to:" + message.to + ",amount:" + message.amount;
+                        console.log(details)
+                        // Set your own code after we find our create account event
+                        stream()
+                    }
                 }
-            }
-        })
+            })
+    }
+    catch (e) {
+        console.log("error occured")
+    }
 });
 
 
@@ -376,70 +381,147 @@ router.get('/streampayments', function (req, res) {
 
 router.get('/recentTransactions', function (req, res) {
     //login account recent 200  transaction max
-    var urll = 'https://horizon-testnet.stellar.org/accounts/' + account2.publicKey + '/operations?order=desc&limit=200';
-    console.log(urll);
-    request.get({
-        url: urll,
-        json: true
-    }, function (error, response, body) {
-        if (error || response.statusCode !== 200) {
-            console.error('ERROR!', error || body);
-        }
-        else {
-            var resp = response.body._embedded.records;
-            var count = 1;
-            for (val in resp) {
-                //for type_i ==1 for payment type_i is 1 in json response from  horizon server
-
-                if (resp[count].type_i == '1') {
-                    console.log(count + '. from :' + resp[count].from + ', to : ' + resp[count].to + ', amount :' + resp[count].amount)
-                }
-                count++;
-
+    try {
+        var urll = urlString + '/accounts/' + account2.publicKey + '/operations?order=desc&limit=200';
+        console.log(urll);
+        request.get({
+            url: urll,
+            json: true
+        }, function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                console.error('ERROR!', error || body);
             }
-            console.log('###################################')
-            res.send(resp);
-        }
-    });
+            else {
+                var resp = response.body._embedded.records;
+                var count = 1;
+                for (val in resp) {
+                    //for type_i ==1 for payment type_i is 1 in json response from  horizon server
+
+                    if (resp[count].type_i == '1') {
+                        console.log(count + '. from :' + resp[count].from + ', to : ' + resp[count].to + ', amount :' + resp[count].amount)
+                    }
+                    count++;
+
+                }
+                console.log('###################################')
+                res.send(resp);
+            }
+        });
+    }
+    catch (e) {
+        console.log("error occured")
+    }
 });
 
 
 
 
 router.get('/getDetailTxHashUsingHash', function (req, res) {
-    // var hash = req.body.hash;
-    var hash = 'cfbd98664e3d3e22230c8a3c5b7ca163789dd0df7b31fc72ca812e050df30a9e';//manage offer
-    //d4fe007dff517b724c402bab959f83f4423b61d2729c973026bd7cf8ab0b54a5 payment hash
-    var url = 'https://horizon-testnet.stellar.org/transactions/' + hash + '/operations';
-   // console.log(url);
+    try {
+        // var hash = req.body.hash;
+        var hash = 'cfbd98664e3d3e22230c8a3c5b7ca163789dd0df7b31fc72ca812e050df30a9e';//manage offer
+        //d4fe007dff517b724c402bab959f83f4423b61d2729c973026bd7cf8ab0b54a5 payment hash
+        var url = urlString + '/transactions/' + hash + '/operations';
+        // console.log(url);
 
-    request.get({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-        if (error || response.statusCode !== 200) {
-            console.error('ERROR!', error || body);
-        }
-        else {
-             if (response.body._embedded.records[0].type == "payment") {
-            //console.log("hellllllo", response.body._embedded.records.from,response.body._embedded.records.to,response.body._embedded.records.amount);
-            console.log('from :'+response.body._embedded.records[0].from)
-            console.log(', to :'+response.body._embedded.records[0].to)
-            console.log(',amount :',response.body._embedded.records[0].amount)
-            console.log(',asset-type :'+response.body._embedded.records[0].asset_type)
-            console.log('#########################')
-           res.send(response.body._embedded.records[0])
-             }
-             else{
-                 //res.send(response.body)
-                 console.log('not a payement transaction');
-                 res.send('not a payement transaction')
-             }
-        }
-    });
+        request.get({
+            url: url,
+            json: true
+        }, function (error, response, body) {
+            if (error || response.statusCode !== 200) {
+                console.error('ERROR!', error || body);
+            }
+            else {
+                if (response.body._embedded.records[0].type == "payment") {
+                    //console.log("hellllllo", response.body._embedded.records.from,response.body._embedded.records.to,response.body._embedded.records.amount);
+                    console.log('from :' + response.body._embedded.records[0].from)
+                    console.log(', to :' + response.body._embedded.records[0].to)
+                    console.log(',amount :', response.body._embedded.records[0].amount)
+                    console.log(',asset-type :' + response.body._embedded.records[0].asset_type)
+                    console.log('#########################')
+                    res.send(response.body._embedded.records[0])
+                }
+                else {
+                    //res.send(response.body)
+                    console.log('not a payement transaction');
+                    res.send('not a payement transaction')
+                }
+            }
+        });
+    }
+    catch (e) {
+        console.log("error occured")
+    }
+});
+
+
+
+
+
+app.get('/setHomeDomain', function (req, res) {
+    // Keys for issuing account
+    var issuingKeys = StellarSdk.Keypair
+        .fromSecret(issuer.secretKey);
+    server.loadAccount(issuingKeys.publicKey())
+        .then(function (issuer) {
+            var transaction = new StellarSdk.TransactionBuilder(issuer)
+                .addOperation(StellarSdk.Operation.setOptions({
+                    homeDomain: 'oktoken.com',//set the homedomain
+                }))
+                .build();
+            transaction.sign(issuingKeys);
+            return server.submitTransaction(transaction);
+        })
+        .catch(function (error) {
+            console.error('Error!', error);
+        });
+});
+
+
+router.get('/test', async function (req, res) {
+    try {
+        var sourceSecretKey = issuer.secret;// seed of acc from which tx process
+        var sourceKeypair = StellarSdk.Keypair.fromSecret(sourceSecretKey);
+        var sourcePublicKey = sourceKeypair.publicKey();//pub key of source acc
+        var receiverPublicKey = account2.publicKey;// public key of reciever acc
+        StellarSdk.Network.useTestNetwork();
+        // console.log('1');
+        //var server = new StellarSdk.Server('https://horizon-testnet.stellar.org');//live netwrk
+
+        //var server = new StellarSdk.Server('http://localhost:8000', { allowHttp: true });
+        //console.log('2');
+
+        const account = await server.loadAccount(sourcePublicKey)
+        //console.log('3');
+
+        var transaction = new StellarSdk.TransactionBuilder(account)
+            .addOperation(StellarSdk.Operation.payment({
+                destination: receiverPublicKey,
+                asset: StellarSdk.Asset.native(),//native asset lumen
+                amount: '0.00004',
+            }))
+            .addMemo(StellarSdk.Memo.text('token sent xlm'))//optional 
+            .build();
+
+        console.log('4');
+
+        transaction.sign(sourceKeypair);
+
+        const temp = await server.submitTransaction(transaction)
+        console.log('submitted', temp._links);
+        console.log('###############################');
+
+        res.send('done');
+
+        //return res.json({ data: temp})
+    } catch (e) {
+        console.log("error occured")
+        // return res.json({ error: e})
+    }
 });
 
 
 module.exports = router;
 
 
+///home/prashant/prashant/stellar-core/src/transactions/readme.md
--- app.js
+++ app.js
@@ -15,15 +15,59 @@ app.use(require('express-jquery')('/jquery.js'));
 var session = require('express-session');
 app.use(session({ secret: 'prashant', saveUninitialized: true, resave: true }));
 //sssion
+
+
+//cors
+var cors = require('cors');
+app.use(cors());
+//
+
+
+
 app.listen(4200, () => {
     console.log("connected port 4200 wallet api");
 })
 
-app.use('/api', require('./api/routes/index.js'));
+const authorizationFlag = require('./api/routes/authorizationFlag.js');
+const balanceOf= require('./api/routes/balanceOf.js');
+const burn= require('./api/routes/burn.js');
+const changeTrust= require('./api/routes/changeTrust.js');
+const checkIsAuthorized= require('./api/routes/checkIsAuthorized.js');
+const freezeBurnAccount= require('./api/routes/freezeBurnAccount.js');
+const getDetailTxHashUsingHash= require('./api/routes/getDetailTxHashUsingHash.js');
+const issueAsset= require('./api/routes/issueAsset.js');
+const loadAccount= require('./api/routes/loadAccount.js');
+const login= require('./api/routes/login.js');
+const recentTransactions= require('./api/routes/recentTransactions.js');
+const setHomeDomain= require('./api/routes/setHomeDomain.js');
+const streampayments= require('./api/routes/streampayments.js');
+const transferOktoken= require('./api/routes/transferOktoken.js');
+const transferXlm= require('./api/routes/transferXlm.js');
 
 
-app.get('/',function(req,res){
+app.use('/authorizationFlag', authorizationFlag);
+app.use('/balanceOf', balanceOf)
+app.use('/burn', burn);
+app.use('/changeTrust', changeTrust)
+app.use('/checkIsAuthorized', checkIsAuthorized);
+app.use('/freezeBurnAccount', freezeBurnAccount);
+app.use('/getDetailTxHashUsingHash', getDetailTxHashUsingHash)
+app.use('/issueAsset', issueAsset)
+app.use('/loadAccount', loadAccount)
+app.use('/login', login);
+app.use('/recentTransactions', recentTransactions)
+app.use('/setHomeDomain', setHomeDomain)
+app.use('/streampayments', streampayments)
+app.use('/setHomeDomain', transferOktoken)
+app.use('/streampayments', transferXlm)
+
+
+app.get('/', function (req, res) {
 
     res.send('use other routes');
-}); 
+});
 
+
+
+
+const login = require('./routes/login');
